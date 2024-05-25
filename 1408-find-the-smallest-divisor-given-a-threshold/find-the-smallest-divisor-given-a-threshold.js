5de31c5c526d4f69ed1bb37fe857adcb96594d43
Shweta Kamal <shwetakamal4714@gmail.com>
2024-05-26 00:41:30 +0530

    Time: 68 ms (58.45%) | Memory: 51.7 MB (83.11%) - LeetSync

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @param {number} threshold
+ * @return {number}
+ */
+
+function sumD(arr, div) {
+    let  n = arr.length;
+    let sum = 0;
+
+    for(let i = 0; i< n; i++) {
+        sum += Math.ceil(arr[i] / div);
+    }
+
+    return sum;
+}
+
+var smallestDivisor = function(nums, threshold) {
+  let n = nums.length;
+  if(n > threshold) return -1;
+
+  let low = 1;
+  let high = Math.max(...nums);
+
+  while(low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if(sumD(nums, mid) <= threshold) {
+        high = mid - 1;
+    }else {
+        low = mid + 1;
+    }
+  }
+    return low;
+
+
+};
+

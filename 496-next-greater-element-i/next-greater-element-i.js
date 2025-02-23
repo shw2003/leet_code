32c7dd5a291c90bae1492e01c1627a96537febc6
Shweta Kamal <shwetakamal4714@gmail.com>
2025-02-23 20:29:23 +0530

    Time: 10 ms (14.37%) | Memory: 54.6 MB (10.03%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+    let stack = [];
+    
+    for(let i = 0; i <nums1.length; i++) {
+        let max = -1;
+
+        for(let j = 0; j < nums2.length; j++) {
+            let index = j;
+
+            if(nums1[i] === nums2[j]){
+                while(index !== nums2.length) {
+                    if(nums2[index] > nums1[i]) {
+                        max = nums2[index];
+                        break;
+
+                    }
+                    index++;
+                }
+            }
+
+        }
+           stack.push(max);
+    }
+return stack;
+};

79a820d2459e7508ea2726784ae6f2ae8fdfd854
Shweta Kamal <shwetakamal4714@gmail.com>
2025-04-01 12:35:12 +0530

    Time: 1561 ms (15.43%) | Memory: 58.2 MB (78.98%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.js
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.js
@@ -4,29 +4,17 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-    const n = nums.length; // Size of the given array
-  const map = new Map();
-  let preSum = 0; // Variable to store the prefix sum
-  let cnt = 0; // Variable to store the count of subarrays
+  let count = 0;
 
-  // Initialize the map with 0 having 1 occurrence
-  map.set(0, 1);
+  for(let i = 0; i< nums.length; i++) {
+    let sum = 0;
 
-  for (let i = 0; i < n; i++) {
-    // Add the current element to the prefix sum
-    preSum += nums[i];
+    for(let j = i; j< nums.length; j++){
+        sum += nums[j];
 
-    // Calculate the value to remove (preSum - k)
-    const remove = preSum - k;
-
-    // If remove is found in the map, it means there is a subarray that sums to k
-    if (map.has(remove)) {
-      cnt += map.get(remove); // Increment count by the number of times remove has occurred
+        if(sum === k) count++;
     }
 
-    // Update the count of the current prefix sum in the map
-    map.set(preSum, (map.get(preSum) || 0) + 1);
   }
-
-  return cnt;
+  return count;
 };

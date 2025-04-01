1ac65e4c5a9be7096cb1a69c42c51175d7ee5a68
Shweta Kamal <shwetakamal4714@gmail.com>
2025-04-01 12:59:00 +0530

    Time: 20 ms (56.16%) | Memory: 65.3 MB (22.28%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.js
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.js
@@ -4,17 +4,21 @@
  * @return {number}
  */
 var subarraySum = function(nums, k) {
-  let count = 0;
+   let count = 0, prefixSum = 0;
+    let prefixSumMap = new Map();
+    prefixSumMap.set(0, 1); // Handle subarrays starting from index 0
 
-  for(let i = 0; i< nums.length; i++) {
-    let sum = 0;
+    for (let num of nums) {
+        prefixSum += num;
 
-    for(let j = i; j< nums.length; j++){
-        sum += nums[j];
+        // Check if (prefixSum - k) exists in map
+        if (prefixSumMap.has(prefixSum - k)) {
+            count += prefixSumMap.get(prefixSum - k);
+        }
 
-        if(sum === k) count++;
+        // Update prefix sum frequency in the map
+        prefixSumMap.set(prefixSum, (prefixSumMap.get(prefixSum) || 0) + 1);
     }
 
-  }
   return count;
 };

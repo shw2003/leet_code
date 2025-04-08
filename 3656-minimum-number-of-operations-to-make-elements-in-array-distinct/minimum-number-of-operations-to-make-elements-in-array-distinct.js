193d4a3a4f6d7a13bf271a67b7227bc00ca678c7
Shweta Kamal <shwetakamal4714@gmail.com>
2025-04-08 10:23:48 +0530

    Time: 1 ms (100.00%) | Memory: 55.3 MB (79.31%) - LeetSync

---
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.js
@@ -0,0 +1,10 @@
+var minimumOperations = function(nums) {
+    const seen = new Set();
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (seen.has(nums[i])) {
+            return Math.floor(i / 3) + 1;
+        }
+        seen.add(nums[i]);
+    }
+    return 0;
+};

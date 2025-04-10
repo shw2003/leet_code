f4520efbb8396304722389813eab8b261b7e4e85
Shweta Kamal <shwetakamal4714@gmail.com>
2025-04-10 13:49:37 +0530

    Time: 50 ms (80.39%) | Memory: 57.9 MB (13.72%) - LeetSync

---
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.js
@@ -0,0 +1,10 @@
+var minOperations = function(nums, k) {
+    if (nums.some(num => num < k)) return -1;
+
+    const greater = new Set();
+    for (let num of nums) {
+        if (num > k) greater.add(num);
+    }
+
+    return greater.size;
+};

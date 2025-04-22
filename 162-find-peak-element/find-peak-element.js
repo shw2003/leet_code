5e2c67625be44a580f9a5d93390f6c6accda0951
Shweta Kamal <shwetakamal4714@gmail.com>
2025-04-22 12:44:15 +0530

    Time: 0 ms (100.00%) | Memory: 53.6 MB (63.43%) - LeetSync

--- 162-find-peak-element/find-peak-element.js
+++ 162-find-peak-element/find-peak-element.js
@@ -3,24 +3,19 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    let n = nums.length;
+  let left  = 0, right = nums.length - 1;
+  
+  while(left < right) {
+    let mid = Math.floor((left + right) / 2);
 
-    if(n===1) return 0;
-    if(nums[0] > nums[1]) return 0;
-    if(nums[n-1] > nums[n-2]) return n-1;
- let low = 1, high = n-2;
-    while(low <= high) {
-        let mid = Math.floor((low + high) / 2);
-
-        if(nums[mid ] > nums[mid - 1] && nums[mid] > nums[mid + 1]){
-            return mid;
-        }
-
-        if(nums[mid] > nums[mid - 1]) {
-            low = mid +1;
-        }else{
-            high = mid - 1;
-        }
+    if(nums[mid] > nums[mid + 1]) {
+        right = mid;
     }
-    return -1;
+     else {
+        left = mid + 1
+     }
+
+
+  }  
+   return left;
 };

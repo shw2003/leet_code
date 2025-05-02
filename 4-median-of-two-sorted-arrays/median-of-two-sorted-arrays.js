6c85f2afff34adc83c5f97215e46fa7e1a273042
Shweta Kamal <shwetakamal4714@gmail.com>
2025-05-02 13:06:08 +0530

    Time: 2 ms (89.00%) | Memory: 58.3 MB (73.11%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -0,0 +1,32 @@
+function findMedianSortedArrays(nums1, nums2) {
+  if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
+
+  let x = nums1.length;
+  let y = nums2.length;
+  let low = 0, high = x;
+
+  while (low <= high) {
+    const partitionX = Math.floor((low + high) / 2);
+    const partitionY = Math.floor((x + y + 1) / 2) - partitionX;
+
+    const maxLeftX = partitionX === 0 ? -Infinity : nums1[partitionX - 1];
+    const minRightX = partitionX === x ? Infinity : nums1[partitionX];
+
+    const maxLeftY = partitionY === 0 ? -Infinity : nums2[partitionY - 1];
+    const minRightY = partitionY === y ? Infinity : nums2[partitionY];
+
+    if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+      if ((x + y) % 2 === 0) {
+        return (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2;
+      } else {
+        return Math.max(maxLeftX, maxLeftY);
+      }
+    } else if (maxLeftX > minRightY) {
+      high = partitionX - 1;
+    } else {
+      low = partitionX + 1;
+    }
+  }
+
+  throw new Error("Input arrays are not sorted.");
+}

a5fddba371f3f8d8a2c78bee5ea5868500c9a2a7
Shweta Kamal <shwetakamal4714@gmail.com>
2025-05-11 13:31:42 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (56.39%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.js
+++ 1293-three-consecutive-odds/three-consecutive-odds.js
@@ -1,6 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
 var threeConsecutiveOdds = function(arr) {
-    for (let i = 0; i <= arr.length - 3; i++) {
-        if (arr[i] % 2 === 1 && arr[i+1] % 2 === 1 && arr[i+2] % 2 === 1) {
+    if (arr.length < 3){
+        return false;
+    }
+    for (let i = 0; i < arr.length - 2; i++){
+        if (arr[i] % 2 !== 0 && arr[i + 1] % 2 !== 0 && arr[i + 2] % 2 !== 0){
             return true;
         }
     }
